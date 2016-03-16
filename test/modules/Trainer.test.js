b35061b1f58dc7b1718ce14693f0be34bbf0fb5a
anpandu <anpandumail@gmail.com>
2016-03-16 11:24:47 +0700

    1st

--- test/modules/Trainer.test.js
+++ test/modules/Trainer.test.js
@@ -8,40 +8,40 @@ var Trainer = require('../../lib/modules/Trainer.js')
 describe('Trainer', function () {
 
   it('appendCleanTokens', function () {
-    var data = jsonfile.readFileSync('./test/fixtures/news10-category.json')
+    var data = jsonfile.readFileSync('./test/fixtures/tweet13.json')
     data = Trainer.appendCleanTokens(data)
-    assert(data.length === 10)
+    assert(data.length === 13)
     data.forEach(function (datum) {
-      assert('category' in datum)
+      assert('label' in datum)
       assert('text' in datum)
       assert('tokens' in datum)
     })
   })
 
   it('getWordFreq', function () {
-    var data = jsonfile.readFileSync('./test/fixtures/news10-category.json')
+    var data = jsonfile.readFileSync('./test/fixtures/tweet13.json')
     data = Trainer.appendCleanTokens(data)
     var freqs = Trainer.getWordFreq(data)
     freqs.forEach(function (f) {
-      assert('category' in f)
+      assert('label' in f)
       assert('freq' in f)
     })
   })
 
   it('getTFIDF', function () {
-    var data = jsonfile.readFileSync('./test/fixtures/news10-category.json')
+    var data = jsonfile.readFileSync('./test/fixtures/tweet13.json')
     data = Trainer.appendCleanTokens(data)
     var freqs = Trainer.getWordFreq(data)
     var tfidfs = Trainer.getTFIDF(freqs)
     tfidfs.forEach(function (f) {
-      assert('category' in f)
+      assert('label' in f)
       assert('tfidf' in f)
       assert(_.isArray(f.tfidf))
     })
   })
 
   it('appendScores', function () {
-    var data = jsonfile.readFileSync('./test/fixtures/news10-category.json')
+    var data = jsonfile.readFileSync('./test/fixtures/tweet13.json')
     var tfidf = jsonfile.readFileSync('./test/fixtures/tfidf10.json')
     data = Trainer.appendScores(data, tfidf)
     data.forEach(function (f) {
@@ -51,7 +51,7 @@ describe('Trainer', function () {
   })
 
   it('train', function () {
-    var data = jsonfile.readFileSync('./test/fixtures/news10-category.json')
+    var data = jsonfile.readFileSync('./test/fixtures/tweet13.json')
     Trainer
       .train(data)
       .then(function (result) {
